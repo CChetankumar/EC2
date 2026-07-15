e4c7d3ec0b2761db4c00c7d8a5bc23e4aff8f5a8
CChetankumar <er.chetanchudasama@gmail.com>
2026-07-15 08:11:45 +0530

    New

---
+++ Age.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EC2
+{
+    class Age
+    {
+        int a;
+        public Age(int aa)
+        {
+            a = aa;
+        }
+        void display()
+        {
+            Console.WriteLine("Age:" + a);
+        }
+        static void Main()
+        {
+            Age aaa = new Age(2);
+            aaa.display();
+
+        }
+    }
+}
